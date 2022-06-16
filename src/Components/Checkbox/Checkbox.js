b0bd9fcc8a5a198db150b0feb7cb8cc92a32c48d
q901fj <q901fj@shinsegae.com>
2022-06-16 17:01:55 +0900

    tsx에서 js로 변환

--- src/Components/Checkbox/Checkbox.js
+++ src/Components/Checkbox/Checkbox.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 export default (props) => {
-    //handleCheck = () => {}
-
-    const { label, id, value, onChange } = props
+    const { label, id, checked, onChange } = props
     return (
         <span className="checkbox">
             <input
                 type="checkbox"
                 id={id}
-                checked={value}
-                value={value}
+                checked={checked}
                 onChange={onChange}
             />
             <label htmlFor={id}>{label}</label>
--- src/Components/Checkbox/Checkboxs.js
+++ src/Components/Checkbox/Checkboxs.js
@@ -1,39 +1,40 @@
-import React from "react";
-import Checkbox from "./Checkbox";
-import "./Checkboxs.css";
+import React from 'react'
+import Checkbox from './Checkbox'
+import './Checkboxs.css'
 
 export default class Checkboxs extends React.Component {
     state = {
         currentSelects: [],
         items: [
-            { title: "Checkbox One", value: false },
-            { title: "Checkbox Two", value: false },
-            { title: "Checkbox Three", value: false },
+            { title: 'Checkbox One', value: false },
+            { title: 'Checkbox Two', value: false },
+            { title: 'Checkbox Three', value: false },
         ],
-    };
+    }
 
     handleCurrentSelects = (label, id, value) => {
         const thisElement = {
             title: label,
             id: id,
             value: !value,
-        };
+        }
         //console.log(this.state.items);
         //console.log(thisElement);
-        const currentArray = this.state.currentSelects;
+        const currentArray = this.state.currentSelects
 
         value
             ? currentArray.push(label)
-            : currentArray.splice(currentArray.indexOf(label), 1);
-
+            : currentArray.splice(currentArray.indexOf(label), 1)
+        console.log(currentArray)
+        console.log(label, id, value)
         this.setState({
             currentSelects: currentArray,
-            items: [thisElement, ...this.state.items],
-        });
-    };
+            items: [thisElement],
+        })
+    }
 
     render() {
-        const { currentSelects, items } = this.state;
+        const { currentSelects, items } = this.state
         return (
             <div>
                 {items.map((item, idx) => (
@@ -47,15 +48,15 @@ export default class Checkboxs extends React.Component {
                                 item.title,
                                 idx,
                                 item.value
-                            );
+                            )
                         }}
                     />
                 ))}
-                <p className='current'>
+                <p className="current">
                     <strong>Current Select :</strong>
                     {currentSelects.toString()}
                 </p>
             </div>
-        );
+        )
     }
 }
