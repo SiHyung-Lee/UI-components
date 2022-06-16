b24592efa1cdcad92890021e41d690b64f40c7b9
sihyung <buzzcatfish@gmail.com>
2022-06-17 02:12:48 +0900

    checkbox 컴포넌트 hook으로 변경

--- src/Components/Checkbox/Checkbox.js
+++ src/Components/Checkbox/Checkbox.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default (props) => {
-    const { label, id, checked, onChange } = props
+    const [checked, setChecked] = useState()
+    const { label, id, onChange } = props
+
+    const handleChange = () => {
+        setChecked(!checked)
+    }
+
     return (
         <span className="checkbox">
             <input
                 type="checkbox"
                 id={id}
                 checked={checked}
-                onChange={onChange}
+                onChange={handleChange}
             />
             <label htmlFor={id}>{label}</label>
         </span>
--- src/Components/Checkbox/Checkboxs.js
+++ src/Components/Checkbox/Checkboxs.js
@@ -1,62 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Checkbox from './Checkbox'
 import './Checkboxs.css'
 
-export default class Checkboxs extends React.Component {
-    state = {
-        currentSelects: [],
-        items: [
-            { title: 'Checkbox One', value: false },
-            { title: 'Checkbox Two', value: false },
-            { title: 'Checkbox Three', value: false },
-        ],
-    }
+export default () => {
+    const [currentSelects, setCurrentSelects] = useState()
+    const [items, setItems] = useState([
+        'Checkbox One',
+        'Checkbox Two',
+        'Checkbox Three',
+    ])
 
-    handleCurrentSelects = (label, id, value) => {
-        const thisElement = {
-            title: label,
-            id: id,
-            value: !value,
-        }
-        //console.log(this.state.items);
-        //console.log(thisElement);
-        const currentArray = this.state.currentSelects
+    const handleCurrentSelects = () => {}
 
-        value
-            ? currentArray.push(label)
-            : currentArray.splice(currentArray.indexOf(label), 1)
-        console.log(currentArray)
-        console.log(label, id, value)
-        this.setState({
-            currentSelects: currentArray,
-            items: [thisElement],
-        })
-    }
-
-    render() {
-        const { currentSelects, items } = this.state
-        return (
-            <div>
-                {items.map((item, idx) => (
-                    <Checkbox
-                        label={item.title}
-                        key={idx}
-                        id={idx}
-                        checked={item.value}
-                        onChange={() => {
-                            this.handleCurrentSelects(
-                                item.title,
-                                idx,
-                                item.value
-                            )
-                        }}
-                    />
-                ))}
-                <p className="current">
-                    <strong>Current Select :</strong>
-                    {currentSelects.toString()}
-                </p>
-            </div>
-        )
-    }
+    return (
+        <div>
+            {items.map((item, idx) => (
+                <Checkbox label={item.title} key={idx} id={idx} />
+            ))}
+            <p className="current">
+                <strong>Current Select :</strong>
+                {currentSelects.toString()}
+            </p>
+        </div>
+    )
 }
