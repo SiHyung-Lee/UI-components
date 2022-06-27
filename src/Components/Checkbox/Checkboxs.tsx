d1afd8760b7ce84cac1c75c887ee7da983c546d3
sihyung <buzzcatfish@gmail.com>
2022-06-28 00:54:34 +0900

    타입스크립트 적용

--- src/Components/Checkbox/Checkboxs.tsx
+++ src/Components/Checkbox/Checkboxs.tsx
@@ -7,9 +7,9 @@ const items = ['Checkbox One', 'Checkbox Two', 'Checkbox Three'];
 export default () => {
     const [selects, setSelects] = useState([]);
 
-    const updateSelects = (label, value) => {
+    const updateSelects = (label: string, value: boolean) => {
         let arraySelects = [...selects];
-
+        console.log(label);
         value
             ? arraySelects.push(label)
             : arraySelects.splice(arraySelects.indexOf(label), 1);
